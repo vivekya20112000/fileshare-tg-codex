88ea8f4442ea6e3816e0b6b64ca215dd5bcd2b7b
vivekya20112000 <38855980+vivekya20112000@users.noreply.github.com>
2024-04-02 19:29:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,6 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
